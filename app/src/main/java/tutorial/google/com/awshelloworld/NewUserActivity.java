1515d7d731a4051fd2f595f613509e2e850a25c9
Michael Brennan <mikiebrenbren@gmail.com>
2015-03-12 20:12:18 -0600

    clean commit, removed aws stuff that was unused

--- app/src/main/java/tutorial/google/com/awshelloworld/NewUserActivity.java
+++ app/src/main/java/tutorial/google/com/awshelloworld/NewUserActivity.java
@@ -3,11 +3,11 @@ package tutorial.google.com.awshelloworld;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
 
 
 public class NewUserActivity extends ActionBarActivity {
@@ -17,6 +17,9 @@ public class NewUserActivity extends ActionBarActivity {
     protected EditText mEmailEditText;
     protected EditText mPasswordEditText;
     protected Button mCreateAccountButton;
+
+    protected CognitoCachingCredentialsProvider mcredentialsProvider;
+
 
 
     @Override
@@ -37,30 +40,9 @@ public class NewUserActivity extends ActionBarActivity {
                 Log.i("Last Name", mLastNameEditText.getText().toString());
                 Log.i("Email", mEmailEditText.getText().toString());
                 Log.i("Password", mPasswordEditText.getText().toString());
+
             }
         });
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_new_user, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
